f3e11f576bb420e97182e1f20075fffdacd17716
kimjongbuem <42004058+kimjongbuem@users.noreply.github.com>
2019-09-15 17:06:33 +0900

    Add files via upload

--- UnOrderedMap.cpp
+++ UnOrderedMap.cpp
@@ -42,39 +42,40 @@ int main() {
 			if (new_count > n_buckets) {
 				cout << "\nBucket count increased to " << new_count << ". Load factor was " << lf << "and is now" << people.load_factor()
 					<< "\nMaxinum elements in a bucket was " << *std::max_element(std::begin(bucket_counts), std::end(bucket_counts)) << endl;
-			}
-			if (n_buckets <= 64) {
-				cout << "Bucket counts before increase were: " << endl;
-				list_bucket_counts(bucket_counts);
-			}
 
-			n_buckets = new_count; //버킷 개수 업데ㅣ트
-			bucket_counts = std::vector<size_t>(n_buckets);// 버켓카운트를 위한 새백터
-			rehash = true;
-		}
-		for (size_t i = 0; i < n_buckets; i++) {
-			bucket_counts[i] = people.bucket_size(i);
-		}
-		if (rehash)
-		{
-			rehash = false;
-			cout << "\nRehashed Container. Buck count is " << n_buckets << ". Element count is " << people.size() << "\nMaxinum element count in a bucket is now"
-				<< *std::max_element(begin(bucket_counts), end(bucket_counts)) << endl;
-			if (n_buckets <= 64) {
-				cout << "\nBucket counts after rehash are:\n";
-				list_bucket_counts(bucket_counts);
+				if (n_buckets <= 64) {
+					cout << "Bucket counts before increase were: " << endl;
+					list_bucket_counts(bucket_counts);
+				}
+
+				n_buckets = new_count; //버킷 개수 업데ㅣ트
+				bucket_counts = std::vector<size_t>(n_buckets);// 버켓카운트를 위한 새백터
+				rehash = true;
+			}
+			for (size_t i = 0; i < n_buckets; i++) {
+				bucket_counts[i] = people.bucket_size(i);
+			}
+			if (rehash)
+			{
+				rehash = false;
+				cout << "\nRehashed Container. Buck count is " << n_buckets << ". Element count is " << people.size() << "\nMaxinum element count in a bucket is now"
+					<< *std::max_element(begin(bucket_counts), end(bucket_counts)) << endl;
+				if (n_buckets <= 64) {
+					cout << "\nBucket counts after rehash are:\n";
+					list_bucket_counts(bucket_counts);
+				}
 			}
 		}
-		cout << "Final state for this container is:\n: " <<
-			"Bucket Count: " << people.bucket_count() << "\t Element count: " << people.size()
-			<< "\t Maximum element count in a bucket: " << *std::max_element(std::begin(bucket_counts), end(bucket_counts)) << endl;
+			cout << "Final state for this container is:\n: " <<
+				"Bucket Count: " << people.bucket_count() << "\t Element count: " << people.size()
+				<< "\t Maximum element count in a bucket: " << *std::max_element(std::begin(bucket_counts), end(bucket_counts)) << endl;
 
-		value = 1;
-		people = unordered_map<string, size_t>();
-		n_buckets = people.bucket_count();
-		bucket_counts = std::vector<size_t>(n_buckets);
-		mlf += 0.25f;
-		people.max_load_factor(mlf);
+			value = 1;
+			people = unordered_map<string, size_t>();
+			n_buckets = people.bucket_count();
+			bucket_counts = std::vector<size_t>(n_buckets);
+			mlf += 0.25f;
+			people.max_load_factor(mlf);
 	}
 	system("pause");
 }
